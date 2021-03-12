81373c67d07b76edd548d2747eb240c0a7e626d4
William <coe9257@gmail.com>
2021-03-12 05:47:26 -0800

    Alert now works with 5 wins on either computer or player. Currently made so player only chooses rock and computer randomly chooses between the three

--- rockPaperScissors.js
+++ rockPaperScissors.js
@@ -2,60 +2,71 @@
 let playerScore = 0;
 let computerScore = 0;
 
+
+let checkScore = function XR21() {
+    if (playerScore === 5) {
+        alert('PlayerWins!');
+    } else if (computerScore === 5) {
+        alert('ComputerWins!');
+    }
+}
+
+
 function game() {
 
-  for (i = 0; i < 30; i++) {
-    console.log(i);
-
-    function playRound() {
-
-      let random10 = function computerPlay() {
+    let computerChoice = function computerPlay() {
         let a = Math.floor(Math.random() * 3 + 1);
         if (a === 1) {
-          a = "rock";
+            a = "rock";
         } else if (a === 2) {
-          a = "paper";
+            a = "paper";
         } else if (a === 3) {
-          a = "scissors";
+            a = "scissors";
         }
         return a;
-      }
-
-      computerSelection = random10();
-      playerSelection = 'rock';
-
-      if (playerSelection === "rock" && computerSelection === "rock") {
-        console.log("It is a tie");
-      } else if (playerSelection === "rock" && computerSelection === "paper") {
-        computerScore++;
-        console.log("Computer Wins!");
-      } else if (playerSelection === "rock" && computerSelection === "scissors") {
-        playerScore++;
-        console.log("Player Wins!");
-      } else if (playerSelection === "paper" && computerSelection === "paper") {
-        playerScore++;
-        console.log("It is a tie");
-      } else if (playerSelection === "paper" && computerSelection === "scissors") {
-        computerScore++;
-        console.log("Computer Wins!");
-      } else if (playerSelection === "paper" && computerSelection === "rock") {
-        playerScore++;
-        console.log("Player Wins!");
-      } else if (playerSelection === "scissors" && computerSelection === "scissors") {
-        playerScore++;
-        console.log("It is a tie");
-      } else if (playerSelection === "scissors" && computerSelection === "rock") {
-        computerScore++;
-        console.log("Computer Wins!");
-      } else if (playerSelection === "scissors" && computerSelection === "paper") {
-        playerScore++;
-        console.log("Player Wins!");
-      }
     }
 
-    playRound();
-  }
+    for (i = 0; i < 15; i++) {
+        checkScore();
+        console.log(i);
+        console.log('playerScore:', playerScore);
+        console.log('computerScore:', computerScore);
 
+        function playRound() {
+
+            computerChoice();
+
+            computerSelection = computerChoice();
+            playerSelection = 'rock';
+
+            if (playerSelection === "rock" && computerSelection === "rock") {
+                console.log("It is a tie");
+            } else if (playerSelection === "rock" && computerSelection === "paper") {
+                computerScore++;
+                console.log("Computer Wins!");
+            } else if (playerSelection === "rock" && computerSelection === "scissors") {
+                playerScore++;
+                console.log("Player Wins!");
+            } else if (playerSelection === "paper" && computerSelection === "paper") {
+                console.log("It is a tie");
+            } else if (playerSelection === "paper" && computerSelection === "scissors") {
+                computerScore++;
+                console.log("Computer Wins!");
+            } else if (playerSelection === "paper" && computerSelection === "rock") {
+                playerScore++;
+                console.log("Player Wins!");
+            } else if (playerSelection === "scissors" && computerSelection === "scissors") {
+                console.log("It is a tie");
+            } else if (playerSelection === "scissors" && computerSelection === "rock") {
+                computerScore++;
+                console.log("Computer Wins!");
+            } else if (playerSelection === "scissors" && computerSelection === "paper") {
+                playerScore++;
+                console.log("Player Wins!");
+            }
+        }
+        playRound();
+    }
 }
 
 console.log(game())
