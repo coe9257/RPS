a1e4ab532637ccb4301bce9f7b8e58459540dac8
William <coe9257@gmail.com>
2021-03-12 17:43:07 -0800

    First finished prototype of game. Will need to update prompt with selection boxes in future along with a page refresh when the game ends

--- rockPaperScissors.js
+++ rockPaperScissors.js
@@ -3,17 +3,38 @@ let playerScore = 0;
 let computerScore = 0;
 
 
-// let checkScore = function XR21() {
-//     if (playerScore === 5) {
-//         alert('PlayerWins!');
-//     } else if (computerScore === 5) {
-//         alert('ComputerWins!');
-//     }
-// }
+//Score board on page
+document.getElementById('pScore').textContent = 0;
+document.getElementById('cScore').textContent = 0;
+
+//Checks to score and restarts the game is one of the players reach 5 wins
+function checkScore(playerScore, computerScore) {
+    let answer;
+    if (playerScore === 5) {
+        answer = prompt('Player Wins! Would you like to play again?')
+    } else if (computerScore === 5) {
+        answer = prompt('Computer Wins! Would you like to play again?')
+    }
+}
+
+//updates the scoareboard when the computer wins a round
+function computerUpdate() {
+    computerScore++;
+    document.getElementById('cScore').textContent = computerScore;
+}
+
+//updates the scoareboard when the player wins a round
+function playerUpdate() {
+    playerScore++;
+    document.getElementById('pScore').textContent = playerScore;
+}
 
 
+//Main game function
 function game() {
 
+
+    //Uses math random for the computer to randomly pick rock, paper or scissors
     let computerChoice = function computerPlay() {
         let a = Math.floor(Math.random() * 3 + 1);
         if (a === 1) {
@@ -26,45 +47,62 @@ function game() {
         return a;
     }
 
-
-    // checkScore();
-
+    //Plays a round after player and computer have chosen
     function playRound() {
 
+        //checks the score to end or restart of necessary
+        checkScore(playerScore, computerScore);
+
+        //computers choice of picking rock paper or scissors
         computerChoice();
 
-        computerSelection = computerChoice();
-        playerSelection = 'rock';
+        let computerSelection = computerChoice();
+        let playerSelection = prompt('Please Pick Rock Paper or Scissors!').toLowerCase();
 
+        //three statements are the updates of who wins the round
+        let playerWins = `Player chose ${playerSelection} and computer chose ${computerSelection} ${playerSelection} wins`;
+        let computerWins = `Computer chose ${computerSelection} and player chose ${playerSelection} ${computerSelection} wins`
+        let tie = `Player chose ${playerSelection} and computer chose ${computerSelection} it is a tie!`;
+
+        //the game process
         if (playerSelection === "rock" && computerSelection === "rock") {
-            console.log("It is a tie");
+            document.getElementById('output').textContent = tie;
+            playRound();
         } else if (playerSelection === "rock" && computerSelection === "paper") {
-            computerScore++;
-            console.log("Computer Wins!");
+            computerUpdate();
+            document.getElementById('output').textContent = computerWins;
+            playRound()
         } else if (playerSelection === "rock" && computerSelection === "scissors") {
-            playerScore++;
-            console.log("Player Wins!");
+            playerUpdate();
+            document.getElementById('output').textContent = playerWins;
+            playRound()
         } else if (playerSelection === "paper" && computerSelection === "paper") {
-            console.log("It is a tie");
+            document.getElementById('output').textContent = tie;
+            playRound()
         } else if (playerSelection === "paper" && computerSelection === "scissors") {
-            computerScore++;
-            console.log("Computer Wins!");
+            computerUpdate();
+            document.getElementById('output').textContent = computerWins;
+            playRound()
         } else if (playerSelection === "paper" && computerSelection === "rock") {
-            playerScore++;
-            console.log("Player Wins!");
+            playerUpdate();
+            document.getElementById('output').textContent = playerWins;
+            playRound()
         } else if (playerSelection === "scissors" && computerSelection === "scissors") {
-            console.log("It is a tie");
+            document.getElementById('output').textContent = tie;
+            playRound()
         } else if (playerSelection === "scissors" && computerSelection === "rock") {
-            computerScore++;
-            console.log("Computer Wins!");
+            computerUpdate();
+            document.getElementById('output').textContent = computerWins;
+            playRound()
         } else if (playerSelection === "scissors" && computerSelection === "paper") {
-            playerScore++;
-            console.log("Player Wins!");
+            playerUpdate();
+            document.getElementById('output').textContent = playerWins;
+            playRound()
         }
     }
+    playRound();
 }
 
-console.log(game())
 
 
 
